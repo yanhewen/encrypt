825cb5c52316419290b4ca979a42cca95731f68a
yanhwg <yan-hewen@163.com>
2018-08-27 18:18:22 +0800

    JCE与BC的实现加密方式

---
+++ encryptdemo/src/main/java/com/dcits/app/utils/GenerateCert.java
@@ -0,0 +1,32 @@
+package com.dcits.app.utils;
+
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.generators.RSAKeyPairGenerator;
+import org.bouncycastle.crypto.params.RSAKeyGenerationParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.crypto.params.RSAPrivateCrtKeyParameters;
+
+import java.math.BigInteger;
+import java.security.*;
+
+public class GenerateCert {
+
+  public KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+    KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+    SecureRandom sr = new SecureRandom();
+    keygen.initialize(2048, sr);
+    KeyPair keyPair = keygen.generateKeyPair();
+    PrivateKey privKey = keyPair.getPrivate();
+    PublicKey pubKey = keyPair.getPublic();
+    System.out.println("privateKey:" + privKey + ",publicKey:" + pubKey);
+    return keyPair;
+  }
+
+  public void generateKeyPair_bc() {
+    RSAKeyPairGenerator gen = new RSAKeyPairGenerator();
+    gen.init(new RSAKeyGenerationParameters(BigInteger.valueOf(3), new SecureRandom(), 2048, 80));
+    AsymmetricCipherKeyPair keyPair = gen.generateKeyPair();
+    RSAKeyParameters pubKey = (RSAKeyParameters) keyPair.getPublic();
+    RSAPrivateCrtKeyParameters priKey = (RSAPrivateCrtKeyParameters) keyPair.getPrivate();
+  }
+}
